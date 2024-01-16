b1b5feb280bfec6b2e647bd1882b93731ed96cf7
tejasvmodi <moditejasv@gmail.com>
2024-01-16 18:17:44 +0530

    commit

---
+++ lib/Screen/Vendors/vendor screen components/vendorbox.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+VendorBox(String Imagelink,String venuename,String venueprovide){
+  return InkWell(
+                child: SizedBox(
+                  width: 370,
+                  height: 100,
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        top: 0,
+                        left: 10,
+                        child: Container(
+                          width: 350,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                  topLeft: Radius.circular(10)),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromRGBO(0, 0, 0, 0.25),
+                                  offset: Offset(0, 1),
+                                  blurRadius: 4,
+                                )
+                              ],
+                              color: const Color.fromRGBO(239, 226, 226, 1),
+                              border: Border.all(
+                                color: const Color.fromRGBO(77, 43, 43, 1),
+                                width: 0.5,
+                              )),
+                          child: Row(
+                            children: [
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                height: 65,
+                                width: 65,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                       Imagelink,
+                                      ),
+                                      fit: BoxFit.cover,
+                                      opacity: 1),
+                                  borderRadius:
+                                      const BorderRadius.all(Radius.elliptical(65, 65)),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 25,
+                              ),
+                              RichText(
+                                text:  TextSpan(
+                                  style: const TextStyle(
+                                    color: Color.fromRGBO(85, 32, 32, 1),
+                                    fontFamily: 'EBGaramond',
+                                    fontSize: 20,
+                                    letterSpacing: 0,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: '$venuename\n',
+                                    ),
+                                    const WidgetSpan(
+                                      child: SizedBox(
+                                          height:
+                                              25), 
+                                    ),
+                                    TextSpan(
+                                      text:
+                                        venueprovide,
+                                             style: const TextStyle(
+                                        fontSize:
+                                            14, 
+                                        fontWeight: FontWeight.normal,
+                                       
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              onTap: () {
+                
+              },
+              );
+}
--- lib/Screen/Vendors/vendorhome.dart
+++ lib/Screen/Vendors/vendorhome.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
+import 'package:wedding_planning_application/Screen/Vendors/vendor%20screen%20components/vendorbox.dart';
 
 class vendorhome extends StatefulWidget {
   const vendorhome({super.key});
@@ -12,133 +12,129 @@ class vendorhome extends StatefulWidget {
 class _vendorhomeState extends State<vendorhome> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-         backgroundColor: const Color.fromRGBO(255, 217, 249, 1),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: Color.fromRGBO(62, 53, 53, 1),
-          ),
-        ),
-        title: const Padding(
-          padding: EdgeInsets.only(left: 75),
-          child: Text(
-            'Vendors',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: Color.fromRGBO(85, 32, 32, 1),
-              fontFamily: 'EBGaramond',
-              fontSize: 30,
-              letterSpacing: 0,
-              fontWeight: FontWeight.bold,
-              height: 1,
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(255, 217, 249, 1),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Color.fromRGBO(62, 53, 53, 1),
             ),
           ),
+          title: const Padding(
+            padding: EdgeInsets.only(left: 75),
+            child: Text(
+              'Vendors',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Color.fromRGBO(85, 32, 32, 1),
+                fontFamily: 'EBGaramond',
+                fontSize: 30,
+                letterSpacing: 0,
+                fontWeight: FontWeight.bold,
+                height: 1,
+              ),
+            ),
+          ),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  MdiIcons.heartOutline,
+                  size: 40,
+                )),
+            const SizedBox(
+              width: 5,
+            )
+          ],
         ),
-        elevation: 0,
-        actions:  [
-          IconButton(onPressed: () {
-            
-          }, icon: Icon(MdiIcons.heartOutline,size: 40,)),
-          const SizedBox(
-            width: 5,
-          )
-        ],
-      ),
-      body:  SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-           decoration: const BoxDecoration(color: Color.fromRGBO(255, 217, 249, 1),),
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Color.fromRGBO(255, 217, 249, 1),
+          ),
           child: Column(children: [
-              SizedBox(
-              
-                height: 60,
-                  child: Container(
-                     decoration:  BoxDecoration(
-                    color: const Color.fromRGBO(255, 217, 249, 1),
-                    border : Border.all(
-            color: const Color.fromRGBO(111, 80, 80, 1),
-            width: 1,
-          ),
+            SizedBox(
+              height: 60,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(255, 217, 249, 1),
+                  border: Border.all(
+                    color: const Color.fromRGBO(111, 80, 80, 1),
+                    width: 1,
                   ),
-
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(onPressed: () {
-                          
-                        }, icon: Icon(MdiIcons.tuneVertical,size: 45,)),
-                      IconButton(onPressed: () {
-                          
-                        }, icon: const Icon(Icons.search,size: 45,)),
-                        IconButton(onPressed: () {
-                          
-                        }, icon: Icon(MdiIcons.bookmarkOutline,size: 45,)),
-                      ],
-                                  ),
-                  ),
-
-              ),
-              const SizedBox(height: 25),
-               SizedBox(
-              width: 370,
-              height: 100,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 0,
-                    left: 10,
-                    child: Container(
-                      width: 350,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
-                              topLeft: Radius.circular(10)),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Color.fromRGBO(0, 0, 0, 0.25),
-                              offset: Offset(0, 1),
-                              blurRadius: 4,
-                            )
-                          ],
-                          color:  const Color.fromRGBO(239, 226, 226, 1),
-                          border: Border.all(
-                            color: const Color.fromRGBO(77, 43, 43, 1),
-                            width: 0.5,
-                          )),
-                      child:  Row(
-                        children: [
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            height: 65,
-                            width: 65,
-                            decoration: BoxDecoration(
-                              // image: DecorationImage(image: 
-                              // )
-                            ),
-                          )
-                          
-                           
-                    
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          MdiIcons.tuneVertical,
+                          size: 45,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          size: 45,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          MdiIcons.bookmarkOutline,
+                          size: 45,
+                        )),
+                  ],
+                ),
               ),
             ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/Vender_Venue.jpg', 'Venue',
+                        'Lawns/Farmhouse,Banquet Halls, Resorts,\nSmall Function Hall,Destination Wedding,\nHotels'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/Vender_Mackup.jpg', 'MakeUp',
+                        'Bridal Makeup,Family Makeup,Jewellery,\nAccessories'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/vender_photography.jpg', 'Photography',
+                        'Pre-Wedding Shoot,Photographers,\nVideographers'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/Vendor_carting.jpg', 'Catering',
+                        'Catering Services,Food Stalls,Bartenders,\nHome Catering, Cake'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/Vendor_bridal_wear.jpg', 'Bridal wear',
+                        'Bridal Lehengas,Silk Sarees,\nWedding Gowns, Trousseau Sarees'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/vendor_Groom_wear.jpg', 'Groom Wear',
+                        'Wedding Suits/Tuxes, Sherwani'),
+                    const SizedBox(height: 25),
+                    VendorBox('assets/images/vendors_wedding_decor.jpg',
+                        'Planning and decor', 'Decorators, Wedding Planners'),
+                    const SizedBox(height: 25),
+                    VendorBox(
+                        'assets/images/vendors_musicandDjs.jpg',
+                        'Music and Dance',
+                        'Wedding Entertainment, Djs,\nChoreographer'),
+                    const SizedBox(height: 25),
+                    VendorBox(
+                        'assets/images/Vendor_invitations.jpg',
+                        'Invites and gifts',
+                        'Invitation gifts, Invitations, Favors,\nTrousseau Packers'),
+                    const SizedBox(height: 25),
+                  ],
+                ),
+              ),
+            )
           ]),
-        )),
-    );
+        ));
   }
 }
